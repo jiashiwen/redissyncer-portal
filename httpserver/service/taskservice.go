543ecb68cd79716a7708f2406fd333b0048ec5a7
jiashiwen <jiashiwen@ZBMac-C02F34Q9MD6M.local>
2021-04-25 17:37:18 +0800

    fix listbytaskname bug

--- httpserver/service/taskservice.go
+++ httpserver/service/taskservice.go
@@ -473,6 +473,8 @@ func GetTaskStatusByName(taskNames []string) []*response.TaskStatusResultByName 
 		}
 	}
 
+	global.RSPLog.Sugar().Info(taskIds)
+
 	for _, v := range GetTaskStatusByIDs(taskIds) {
 		taskStatusByName := response.TaskStatusResultByName{
 			TaskName:   v.TaskStatus.TaskName,
@@ -481,7 +483,7 @@ func GetTaskStatusByName(taskNames []string) []*response.TaskStatusResultByName 
 		}
 		taskStatusByNameArray = append(taskStatusByNameArray, &taskStatusByName)
 	}
-	global.RSPLog.Sugar().Info("GetTaskStatusByName")
+
 	global.RSPLog.Sugar().Info(taskStatusByNameArray)
 	return taskStatusByNameArray
 }
